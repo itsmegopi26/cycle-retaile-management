104ddcd2a0bb5841d2b837c5c5274dc61dcd4562
itsmegopi26 <87372041+itsmegopi26@users.noreply.github.com>
2021-07-13 07:18:43 -0700

    Update cyclemanagementcontroller.js

--- Yuloo_rest/cycle_File/cyclemanagementcontroller.js
+++ Yuloo_rest/cycle_File/cyclemanagementcontroller.js
@@ -31,7 +31,7 @@ push.post('/Entry_cycle',urlencodedParser, function (req, res) {
 	    	},function(err){
 	    	    return res.status(500).send("Amount updation failed")
 	    	});
-			//finish the program
+			//finish the program end.
 			
 
 		} else{
